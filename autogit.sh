22981360d9ed6de3f48d9a8679cec142ae0e5935
Aman9026 <amangupta884036@gmail.com>
2020-08-24 15:46:11 +0530

    cleaning the bottleneck

---
+++ autogit.sh
@@ -0,0 +1,35 @@
+[ $# -eq 1 ] && start=$1
+
+if [ ! -d $start ]
+then 
+echo "$start not a directory"
+exit 1
+fi
+dr=`pwd`
+Dir=$(find "$start" -type d)
+echo $Dir
+
+for d in $Dir
+do
+
+cd $dr/$d
+
+arry=(`ls -p | grep -v / `)
+len=${#arry[*]}
+i=0
+while [ $i -lt $len ]; do
+
+git add ${arry[$i]}
+test= git commit -m "Added ${arry[$i]}"
+sleep 60
+
+if [ "$test" == "1" ]; then
+echo "Not executed properly" >&2
+exit 1
+fi
+
+
+let i++
+done
+
+done
---
+++ white.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+find . -name '* *' | while read fname #to read all the files having space in there name
+
+do
+        new_fname=`echo $fname | tr " " "_"` #tr is used here to replace space with an underscore
+
+        if [ -e $new_fname ]
+        then
+                echo "File $new_fname already exists. Not replacing $fname"
+        else
+                echo "Creating new file $new_fname to replace $fname"
+                mv "$fname" $new_fname
+        fi
+done
